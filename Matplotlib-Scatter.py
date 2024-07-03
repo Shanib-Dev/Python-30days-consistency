6bc6e26b9bf966a732ec20e59290e94bcb939d25
Shaikh Sanib <67518992+Shanib-Dev@users.noreply.github.com>
2024-07-03 07:23:53 +0530

    Update Matplotlib-Scatter.py

--- Matplotlib-Scatter.py
+++ Matplotlib-Scatter.py
@@ -87,3 +87,35 @@ colors = np.array([0, 10, 20, 30, 40, 45, 50, 55, 60, 70, 80, 90, 100])
 plt.scatter(x, y, c=colors, cmap='viridis')
 plt.colorbar()
 plt.show()
+
+"Size"
+#You can change the size of the dots with the s argument.
+#Just like colors, make sure the array for sizes has the same length as the arrays for the x- and y-axis:
+#Example
+#Set your own size for the markers:
+
+size = np.array([20,50,100,200,500,1000,60,90,10,300,600,800,75])
+plt.scatter(x, y, s=size)
+#plt.show()
+
+"Alpha"
+#You can adjust the transparency of the dots with the alpha argument.
+#Just like colors, make sure the array for sizes has the same length as the arrays for the x- and y-axis:
+#Example
+#Set your own size for the markers:
+
+plt.scatter(x, y, s=size, alpha=0.5)
+#plt.show()
+
+#Combine Color Size and Alpha
+#You can combine a colormap with different sizes of the dots. This is best visualized if the dots are transparent:
+#Example
+#Create random arrays with 100 values for x-points, y-points, colors and sizes:
+
+x = np.random.randint(100, size=(100))
+y = np.random.randint(100, size=(100))
+colors = np.random.randint(100, size=(100))
+sizes = np.random.randint(100, size=(100))
+
+plt.scatter(x, y, s=sizes, c=colors, alpha=0.5 , cmap='nipy_spectral')
+plt.show()
