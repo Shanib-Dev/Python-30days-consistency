93d061d815ee93ca7250c1594b645fe35c76dbcf
Shaikh Sanib <67518992+Shanib-Dev@users.noreply.github.com>
2024-07-12 23:55:06 +0530

    Add files via upload

---
+++ MongoDB-Drop Collection.py
@@ -0,0 +1,10 @@
+"Delete Collection"
+#You can delete a table, or collection as it is called in MongoDB, by using the drop() method.
+#Example
+#Delete the "customers" collection:
+import pymongo
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customer1"]
+mycol.drop()
+#The drop() method returns true if the collection was dropped successfully, and false if the collection does not exist.
